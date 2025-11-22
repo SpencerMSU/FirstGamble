d17f5b1a4a10d0414624c555f5ab65f0e7581400
SpencerMSU <rjdcbxxhcx@gmail.com>
2025-11-22 19:50:35 +0300

    Init

---
+++ app.js
@@ -0,0 +1,17 @@
+// Логика для обработки кнопок
+document.getElementById("button1").addEventListener("click", function() {
+    console.log("Кнопка 1 нажата");
+    // Здесь можно будет сделать переход на страницу или открыть модальное окно
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    console.log("Кнопка 2 нажата");
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+    console.log("Кнопка 3 нажата");
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+    console.log("Кнопка 4 нажата");
+});
